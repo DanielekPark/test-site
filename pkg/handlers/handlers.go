d28ffa08ca1d4e5303278bbd16abd7bfc5d99fb3
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2025-10-14 18:37:54 -0700

    added functions and html page

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -40,12 +40,79 @@ func FetchEvents(url string, token string) models.EventsPageData {
 
 	defer res.Body.Close()
 	body, _ := io.ReadAll(res.Body)
+	//add catch code here
 
-	//Parses JSON
 	var result models.EventsPageData
 	if err := json.Unmarshal(body, &result); err != nil {
 		fmt.Println("Unable to marshal JSON")
+		//Parse JSON
+		jsonFile, err := os.Open("static/json/event-error-message.json")
+		if err != nil {
+			fmt.Println("Unable to parse json error message")
+		} else {
+			fmt.Println(jsonFile)
+		}
+		/*
+			write code to reassign result to display error message for user
+			create json file based on models file
+		*/
 	}
+	return result
+}
+
+func FetchTribeMembers(url string, token string) models.TribeMembers {
+	godotenv.Load()
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var result models.TribeMembers
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Println("Unable to marshal JSON")
+	}
+	return result
+}
+
+func FetchMembershipDocs(url string, token string) models.ProgramDocs {
+	godotenv.Load()
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var result models.ProgramDocs
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Println("Unable to marshal JSON")
+	}
+
+	return result
+}
+
+func FetchProgramDocs(url string, token string) models.ProgramDocs {
+	godotenv.Load()
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var result models.ProgramDocs
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Println("Unable to marshal JSON")
+	}
+
 	return result
 }
 
@@ -59,24 +126,29 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	if err := tmpl.Execute(w, nil); err != nil {
 		log.Fatal("Failed to parse template ", err)
 	}
+
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
+	token := os.Getenv("TRIBE_MEMBERS_BEARER_TOKEN")
+	url := os.Getenv("ABOUT_ENDPOINT")
+	ParsedData := FetchTribeMembers(url, token).Data
+
 	if err := checkPath("/about", w, r); err != nil {
 		log.Fatal(err)
 	}
 
 	tmpl, _ := template.ParseFiles("static/templates/about.html")
 
-	if err := tmpl.Execute(w, nil); err != nil {
+	if err := tmpl.Execute(w, ParsedData); err != nil {
 		log.Fatal("Failed to parse template ", err)
 	}
 }
 
 func Events(w http.ResponseWriter, r *http.Request) {
-	token := os.Getenv("BEARER_TOKEN")
+	token := os.Getenv("EVENTS_BEARER_TOKEN")
 	url := os.Getenv("EVENTS_ENDPOINT")
-	ParsedData := FetchEvents(url, token).Data
+	ParsedData := FetchEvents(url, token)
 
 	if err := checkPath("/events", w, r); err != nil {
 		log.Fatal(err)
@@ -86,9 +158,7 @@ func Events(w http.ResponseWriter, r *http.Request) {
 	if err := tmpl.Execute(w, ParsedData); err != nil {
 		log.Fatal("Failed to parse template ", err)
 	}
-
-	/*
-		https://stackoverflow.com/questions/38190622/range-within-range-golang-template
-		https://pkg.go.dev/html/template
-	*/
 }
+
+func Membership(w http.ResponseWriter, r *http.Request) {}
+func Programs(w http.ResponseWriter, r *http.Request)   {}
